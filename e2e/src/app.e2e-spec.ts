073a4b356600daccacd1d6ce98b669c736f79803
Christian gomez <chrisystems@hotmail.com>
2020-12-04 02:05:02 +0000

    Updated app.e2e-spec.ts

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ng-azure-devops app is running!');
+    expect(page.getTitleText()).toEqual('fallo ejecución');
   });
 
   afterEach(async () => {
