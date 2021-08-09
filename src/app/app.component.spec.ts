cef48b53e88fdb5056a5b0784e6ff3efb9f51aa8
Christian gomez <chrisystems@hotmail.com>
2021-08-09 22:32:25 +0000

    Updated app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'ng-azure-devops'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-azure-devop');
+    expect(app.title).toEqual('ng-azure-devops');
   });
 
   it('should render title', () => {
